83e6d665c3340c7cd8af8c0c295a4c7d26b1842f
Ryan <ryan@Ryans-MacBook-Pro-4.local>
2020-01-24 11:45:38 -0600

    11pm

---
+++ scripts.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  // Turn Counter
+  let turnCounter = 1;
+
+  // button
+  let button = document.createElement('button');
+  let btnTxt = document.createTextNode('Add Square');
+
+  button.appendChild(btnTxt);
+  document.body.appendChild(button);
+
+  let container = document.createElement('div');
+  container.classList.add('container')
+  document.body.appendChild(container)
+
+
+  // Black Box on Click
+  button.addEventListener('click', () => {
+    let square = document.createElement('div');
+    let boxId = document.createTextNode(`${turnCounter}`)
+    let boxH1 = document.createElement('h1')
+
+    square.classList.add('square')
+    boxH1.classList.add('hide')
+    square.setAttribute('id', `${turnCounter}`);
+    turnCounter++
+
+    boxH1.appendChild(boxId);
+    square.appendChild(boxH1);
+    container.appendChild(square);
+
+
+
+
+
+  })
+
+
+
+
+
+
+
+
+
+})
